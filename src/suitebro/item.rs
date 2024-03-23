e4fa4c9b0f71ffd45d84bc19d55b40749d486252
bree <me@bree.dev>
2024-03-23 12:56:43 -0400

    reduce seeks for `TinyRick`

--- src/suitebro/item.rs
+++ src/suitebro/item.rs
@@ -1,4 +1,4 @@
-use binrw::{binrw, BinRead, BinWrite};
+use binrw::{binrw, BinRead, BinWrite, VecArgs};
 use serde::{Deserialize, Serialize};
 
 use crate::gvas::types::{FString, Quat, Vector, GUID};
@@ -13,41 +13,114 @@ pub struct Item {
     pub tinyrick: TinyRick,
 }
 
-fn default_unk_version_1() -> u32 {
+fn default_format_version() -> u32 {
     1
 }
 
-fn default_unk_version_2() -> u32 {
+fn default_unreal_version() -> u32 {
     517
 }
 
-#[binrw]
+const TINYRICK_MAGIC: &[u8; 8] = b"tinyrick";
+
+// #[binrw]
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[brw(magic = b"tinyrick")]
+// #[brw(magic = b"tinyrick")]
 pub struct TinyRick {
     /// may be some kind of version
     #[serde(skip_serializing)]
-    #[serde(default = "default_unk_version_1")]
-    pub unk_version_1: u32,
+    #[serde(default = "default_format_version")]
+    pub format_version: u32,
     /// may be some kind of version
     #[serde(skip_serializing)]
-    #[serde(default = "default_unk_version_2")]
-    pub unk_version_2: u32,
+    #[serde(default = "default_unreal_version")]
+    pub unreal_version: u32,
 
     pub properties: PropertyMap,
 
     // another count?
     pub unk_count: u32,
 
-    #[br(temp)]
-    #[bw(calc = property_sections.len() as u32)]
-    pub property_section_count: u32,
-    #[br(count = property_section_count)]
+    // #[br(temp)]
+    // #[bw(calc = property_sections.len() as u32)]
+    // pub property_section_count: u32,
+    // #[br(count = property_section_count)]
     pub property_sections: Vec<PropertySection>,
 
     pub rotation: Quat,
     pub position: Vector,
     pub scale: Vector,
+}
+
+impl BinRead for TinyRick {
+    type Args<'a> = ();
+
+    fn read_options<R: std::io::prelude::Read + std::io::prelude::Seek>(
+        reader: &mut R,
+        endian: binrw::Endian,
+        args: Self::Args<'_>,
+    ) -> binrw::prelude::BinResult<Self> {
+        // magic
+        let magic = <[u8; 8]>::read_options(reader, endian, args)?;
+
+        if &magic != TINYRICK_MAGIC {
+            Err(binrw::Error::BadMagic {
+                pos: reader.stream_position()?,
+                found: Box::new(magic),
+            })?;
+        }
+
+        let format_version = u32::read_options(reader, endian, args)?;
+        let unreal_version = u32::read_options(reader, endian, args)?;
+        let properties = PropertyMap::read_options(reader, endian, args)?;
+        let unk_count = u32::read_options(reader, endian, args)?;
+        let property_section_count = u32::read_options(reader, endian, args)?;
+        let property_sections = <Vec<PropertySection>>::read_options(
+            reader,
+            endian,
+            VecArgs {
+                count: property_section_count as usize,
+                inner: (),
+            },
+        )?;
+        let rotation = Quat::read_options(reader, endian, args)?;
+        let position = Vector::read_options(reader, endian, args)?;
+        let scale = Vector::read_options(reader, endian, args)?;
+
+        Ok(Self {
+            format_version,
+            unreal_version,
+            unk_count,
+            properties,
+            property_sections,
+            rotation,
+            position,
+            scale,
+        })
+    }
+}
+
+impl BinWrite for TinyRick {
+    type Args<'a> = ();
+
+    fn write_options<W: std::io::prelude::Write + std::io::prelude::Seek>(
+        &self,
+        writer: &mut W,
+        endian: binrw::Endian,
+        args: Self::Args<'_>,
+    ) -> binrw::prelude::BinResult<()> {
+        let property_section_count = self.property_sections.len() as u32;
+
+        TINYRICK_MAGIC.write_options(writer, endian, args)?;
+        self.format_version.write_options(writer, endian, args)?;
+        self.unreal_version.write_options(writer, endian, args)?;
+        self.properties.write_options(writer, endian, args)?;
+        self.unk_count.write_options(writer, endian, args)?;
+        property_section_count.write_options(writer, endian, args)?;
+        self.rotation.write_options(writer, endian, args)?;
+        self.position.write_options(writer, endian, args)?;
+        self.scale.write_options(writer, endian, args)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
